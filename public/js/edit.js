53c70a7c823cbdb14f5fe7b4b15346bbbfcceda0
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-03-11 21:41:02 -0500

    added values for task title, task description, and task status

--- public/js/edit.js
+++ public/js/edit.js
@@ -4,14 +4,16 @@ const id = window.location.toString().split('/')[window.location.toString().spli
 const editHandler = async (event) => {
     event.preventDefault();
     const title = document.querySelector('input[name="task-title"]').value;
-    const content = document.querySelector('textarea[name="task-content"]').value;
+    const description = document.querySelector('input[name="task-description"]').value; 
+    const status = document.querySelector('textarea[name="task-status"]').value;
 
     const response = await fetch(`/api/task/${id}`, {
         method: 'PUT',
         body: JSON.stringify({
             taskId: id,
             title,
-            content,
+            description,
+            status,
         }),
         headers: {'Content-Type': 'application/json'},
     });
