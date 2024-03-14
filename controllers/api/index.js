c0446e7f87354a1ece953932dfb71c600f44b363
Joshua <Joshuahale829@gmail.com>
2024-03-14 18:53:54 -0500

    Had an error -> fixed it and repushing.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,16 +2,17 @@
 const router = require('express').Router();
 
 // Import required modules
-const mainPage = require('../mainPage');
-const editTask = require('./edit-Task');
-const login = require('./login');
-const newTask = require('./new-task');
-const signup = require('./signup');
-
+const mainPage = require('../mainPage'); // Goes back to mainPage.js in the controllers folder
+const editTask = require('./editTask-routes');
+const homepage = require('./homePage-routes');
+const login = require('./login-routes');
+const newTask = require('./newTask-routes');
+const signup = require('./signup-routes');
 
 // Setup routes
 router.use('../mainPage', mainPage);
 router.use('./editTask', editTask);
+router.use('./homepage', homepage);
 router.use('./login', login);
 router.use('./newTask', newTask);
 router.use('./signup', signup);
