a6b0c6ee644798d7c1cbcd44c7288bd8a3f98835
steve <stevemberry2@gmail.com>
2024-03-14 18:47:00 -0500

    fixed syntax errors

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,19 +2,18 @@
 const router = require('express').Router();
 
 // Import required modules
-const mainPage = require('../mainPage');
-const editTask = require('./edit-Task');
-const login = require('./login');
-const newTask = require('./new-task');
-const signup = require('./signup');
-
+const mainPage = require('../mainPage.js');
+const editTask = require('./edit-Task.js');
+const login = require('./login.js');
+const newTask = require('./new-task.js');
+const signup = require('./signup.js');
 
 // Setup routes
-router.use('../mainPage', mainPage);
-router.use('./editTask', editTask);
-router.use('./login', login);
-router.use('./newTask', newTask);
-router.use('./signup', signup);
+router.use('/mainPage', mainPage);
+router.use('/editTask', editTask);
+router.use('/login', login);
+router.use('/newTask', newTask);
+router.use('/signup', signup);
 
 // Export the router
 module.exports = router;
