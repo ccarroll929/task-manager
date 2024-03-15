a61f166a5701f6bd4d3086d8c064092fd79a36d1
Joshua <Joshuahale829@gmail.com>
2024-03-14 19:31:22 -0500

    Rewrote api->index.js file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,23 +2,20 @@
 const router = require('express').Router();
 
 // Import required modules
-
-const mainPage = require('../mainPage.js');
-const editTask = require('./editTask-routes.js');
-const homepage = require('./homepage-routes.js');
-const login    = require('./login-routes.js');
-const newTask  = require('./newTask-routes.js');
-const signup   = require('./signup-routes.js');
-
+const mainPage = require('./mainPage.js'); 
+const editTask = require('./api/editTask-routes.js'); 
+const homepage = require('./api/homepage-routes.js'); 
+const login    = require('./api/login-routes.js'); 
+const newTask  = require('./api/newTask-routes.js'); 
+const signup   = require('./api/signup-routes.js'); 
 
 // Setup routes
-router.use('../mainPage', mainPage);
-router.use('./editTask', editTask);
-router.use('./homepage', homepage);
-router.use('./login', login);
-router.use('./newTask', newTask);
-router.use('./signup', signup);
-
+router.use('/', mainPage);
+router.use('/editTask', editTask);
+router.use('/homepage', homepage);
+router.use('/login', login);
+router.use('/newTask', newTask);
+router.use('/signup', signup);
 
 // Export the router
 module.exports = router;
