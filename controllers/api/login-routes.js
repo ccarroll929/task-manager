67f07a5edf2a81323cd181f4263e92c6a2a01b36
ccarroll929 <146889005+ccarroll929@users.noreply.github.com>
2024-03-17 11:26:14 -0500

    made changes to login page structure and logic

--- controllers/api/login-routes.js
+++ controllers/api/login-routes.js
@@ -4,7 +4,7 @@ const { User } = require('../../models');
 // Login for existing user
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { email: req.body.username } });
     if (!userData) {
       res.status(400).json({ message: 'Incorrect email or password, please try again' });
       return;
