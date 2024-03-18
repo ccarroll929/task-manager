423c072407fc81df87c039c3f4b8a44aaf599233
Ray <ray@rayxis.com>
2024-03-18 13:24:21 -0500

    Update task-routes.js and user-routes.js with new route methods
    These changes introduce new methods for interacting with Google Tasks within task-routes.js and user-routes.js. The methods allow the retrieval, creation, update and delete operations on task lists, further enhancing the backend's capability to manage tasks.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,50 +1,57 @@
 // Import the router and the User model
 const router = require('express').Router();
-const { User } = require('../../models');
+const {User} = require('../../models');
+
+const googleTasks = require('../../services/GoogleTasksService');
+const redis       = require('../../utils/redis');
 
 // Route to get all users
 router.get('/', (req, res) => {
-    // Access our User model and run .findAll() method
-    User.findAll({ attributes: { exclude: ['password'] } })
-        .then(dbUserData => res.json(dbUserData))
-        .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-        });
-    }
+	           // Access our User model and run .findAll() method
+	           User.findAll({attributes: {exclude: ['password']}})
+	               .then(dbUserData => res.json(dbUserData))
+	               .catch(err => {
+		               console.log(err);
+		               res.status(500).json(err);
+	               });
+           }
 );
 
 // SignUp for new user
 router.post('/', async (req, res) => {
-    try {
-      // Check if the request body contains the required fields
-      if (!req.body.username || !req.body.password) {
-        return res.status(400).json({ message: 'Username and password are required' });
-      }
-  
-      // Create the user with the provided data
-      const userData = await User.create(req.body);
-  
-      // Assuming userData has the password field, sanitize it before sending the response
-      const sanitizedUserData = { ...userData.toJSON() };
-      delete sanitizedUserData.password;
-  
-      // Save session data
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      req.session.user = sanitizedUserData;
-  
-      // Respond with success message and user data
-      return res.status(200).json({
-        user: sanitizedUserData,
-        message: 'Signup successful',
-        redirect: '/homepage'
-      });
-    } catch (err) {
-      // Log and handle errors
-      console.log(err);
-      return res.status(400).json({ error: err.message || 'An error occurred' });
-    }
+	try {
+		// Check if the request body contains the required fields
+		if (!req.body.username || !req.body.password) {
+			return res.status(400).json({message: 'Username and password are required'});
+		}
+
+		// Create the user with the provided data
+		const userData = await User.create(req.body);
+
+		// Assuming userData has the password field, sanitize it before sending the response
+		const sanitizedUserData = {...userData.toJSON()};
+		delete sanitizedUserData.password;
+
+		// Save session data
+		req.session.user_id   = userData.id;
+		req.session.logged_in = true;
+		req.session.user      = sanitizedUserData;
+
+		// Create a new Task List
+		// TODO: Finish this
+		// googleTasks.createTaskList(req.session.user.username);
+
+		// Respond with success message and user data
+		return res.status(200).json({
+			                            user:     sanitizedUserData,
+			                            message:  'Signup successful',
+			                            redirect: '/homepage'
+		                            });
+	} catch (err) {
+		// Log and handle errors
+		console.log(err);
+		return res.status(400).json({error: err.message || 'An error occurred'});
+	}
 });
 
 // Login for existing user
